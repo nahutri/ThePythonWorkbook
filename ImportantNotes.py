c813811fd5d451304147725294bdc5c3a937b5bd
nahutri <ngmyuni@gmail.com>
2018-10-07 11:45:58 +0200

    added more code

--- ImportantNotes.py
+++ ImportantNotes.py
@@ -4,3 +4,9 @@ print("The area of the room is", area,"square feet")
 
 #exercise5,note the %.2f format
 print("your total refund will be $%.2f." %refund)
+
+#exercise6,note the %.2f format
+print("The tax is %.2f and the tips is %.2f, making the total %.2f" (tax,tip,total))
+
+#exercise 7, note the comma
+print("The sum of the first",n,"positive integers is",sm)
