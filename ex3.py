ac8e5b745ffe61082f0891b01d172f30ce208a6a
nahutri <ngmyuni@gmail.com>
2018-10-07 11:06:44 +0200

    first commit

---
+++ ex3.py
@@ -0,0 +1,21 @@
+"""
+Compute the area of a room
+"""
+
+# Read the input values from the user
+
+#My solution
+width=float(input('Enter the width of the room'))
+length=float(input('Enter the length of the room'))
+area=width*length
+print('The area of the room is '+str(area)+'.')
+
+#solution of the book (pattern solution)
+
+length=float(input("Enter the length of room in feet: "))
+width=float(input('Enter the width of the room in feet: '))
+
+#compute the area of the room
+area=width*length
+
+#
